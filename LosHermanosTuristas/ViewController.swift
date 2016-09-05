74957f9e35ae8262279299b4c16c007e69df6989
Araujo, Murilo <muritavo@outlook.com>
2016-09-05 10:58:20 -0300

    Pins gerados

--- LosHermanosTuristas/ViewController.swift
+++ LosHermanosTuristas/ViewController.swift
@@ -20,18 +20,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var currentLocation = CLLocation()
     let regionRadius: CLLocationDistance = 1000
     var isZoomed: Bool = false
+    var pins: [MockPhoto] = [MockPhoto]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if MockPhotoDAO.buscarTodos().count == 0 {
             MockPhotoDAO.loadDB()
+            pins = MockPhotoDAO.buscarTodos()
         }
         
         self.setupLocation()
         self.checkLocationAuth()
         self.setRadius(currentLocation)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createPins()
     }
 
     override func didReceiveMemoryWarning() {
@@ -83,7 +87,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    
+    func createPins() {
+        //Para cada uma das fotos
+        for photo in pins {
+            //Cria uma localizacao com base na latitude e longitude da foto
+            var coord: CLLocationCoordinate2D = CLLocationCoordinate2D()
+            coord.latitude = (photo.latitude)!
+            coord.longitude = (photo.longitude)!
+            
+            //Cria uma anotation
+            let annotation: MKPointAnnotation = MKPointAnnotation()
+            annotation.coordinate = coord
+            annotation.title = photo.title
+            annotation.subtitle = photo.descr
+
+            meuMapa.addAnnotation(annotation)
+        }
+    }
 
 }
 
